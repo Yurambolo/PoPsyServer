7ddfe6b5970fe68534fc44643a03a9d0d55e2447
Yurambolo <61801166+Yurambolo@users.noreply.github.com>
2021-05-09 15:27:47 +0300

    A little changes for deployment

--- PoPsyServer/settings.py
+++ PoPsyServer/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-7!3ibi)vk#tliyuu_-4uf4!g2=t5has-8e%hush_ilzx1(akta')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
