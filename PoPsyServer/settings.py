5e6418bf0d3b62647e5d762d2b7a0c4e9f52c0b4
Yurambolo <61801166+Yurambolo@users.noreply.github.com>
2021-05-12 23:50:13 +0300

    settings fix

--- PoPsyServer/settings.py
+++ PoPsyServer/settings.py
@@ -142,8 +142,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'] = dj_database_url.config()
 DATABASES['default']=db_from_env
-#DATABASES['default'] = dj_database_url.config()
+#
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
